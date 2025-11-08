8439b785e86b3e36ea5ff6d56f1cdcc1d9839425
Roberto <robertovitortroca766@gmail.com>
2025-11-08 16:51:37 -0300

    fazendo a parte de login responder a um usuario existente

--- scripts/parceiro.js
+++ scripts/parceiro.js
@@ -26,3 +26,45 @@ document.addEventListener("DOMContentLoaded", () => {
     // Preenche automaticamente ao abrir a página
     preencherTabela();
 });
+
+//código para a área de login
+document.getElementById("formLogin").addEventListener("submit", function(event){
+    event.preventDefault(); //impede o recarregamento da página
+
+    const User = document.getElementById("User").value;
+    const password = document.getElementById("Password").value;
+
+    //busca de arquivos Json (fetch)
+
+    fetch("login.json")
+        .then(response =>{
+            if(!response.ok){
+                throw new Error("errp ao carregar dados de usuários");
+        }
+        return response.json();
+    });
+
+        .then(usuarios =>{
+            //procura o User correspondente
+            const UserEncontrado = usuarios.find(
+                u => u.User === email && u.password === senha
+            );
+            if (usuarioEncontrado){
+                msg.textContent = 'bem vindo, ${usuarioEncontrado.User}!';
+                msg.style.color = "green";
+
+                //armazena o usuario logado no localStorage
+                localStorage.setItem("usuarioLogado", JSON.stringify(usuarioEncontrado));
+                
+            }
+            else{
+                msg.textContent = "Usuário ou senha incorretos.";
+                msg.style.color = "red";
+            }
+
+            .catch(error=>{
+                msg.textContent = error.message;
+                msg.style.color = "red";
+            });
+        });
+});
